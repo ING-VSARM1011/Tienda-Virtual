9eac98c68922e00cbf474b09a5b70b8d78f57ecb
LAPTOP-VUQ1J6NP\Victor_Moreno <victorsahir1011@gmail.com>
2023-02-09 15:00:58 -0500

    feat: Se agrega archivo del diagrama ER de la base de datos.

--- E_Commerce/settings.py
+++ E_Commerce/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'E-Commerce',
         'USER': 'postgres',
-        'PASSWORD': '1xTHREVUDEER',
+        'PASSWORD': '*********',
         'HOTS': '127.0.0.1',
         'PORT': '5434'
     }
